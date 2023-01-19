ea57d83c554ba20d19a3843aa6214092c79fd445
Esther Kim <kim.esther117@gmail.com>
2023-01-19 14:56:04 -0800

    Built Hat model and view functions

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    section_number = models.PositiveSmallIntegerField()
+    shelf_number = models.PositiveSmallIntegerField()
+    import_href = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.closet_name
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    picture_url = models.URLField(null=True)
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.style_name
+
+    def get_api_url(self):
+        return reverse("api_show_hat", kwargs={"pk": self.pk})
