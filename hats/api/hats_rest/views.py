927684198fba8ae46e173876c8b8b77429b03624
Esther Kim <kim.esther117@gmail.com>
2023-01-19 18:16:27 -0800

    Added api_show_hat view function

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -71,3 +71,35 @@ def api_list_hats(request, location_vo_id=None):
             encoder=HatDetailEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["DELETE", "GET", "PUT"])
+def api_show_hat(request, pk):
+    if request.method == "GET":
+        hat = Hat.objects.get(id=pk)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Hat.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        try:
+            if "location" in content:
+                location = LocationVO.objects.get(location=content["location"])
+                content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location"},
+                status=400,
+            )
+        LocationVO.objects.filter(id=pk).update(**content)
+        hat = Hat.objects.get(id=pk)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False,
+        )
