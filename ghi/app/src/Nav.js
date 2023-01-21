480373159b8668814dd1bd1ee6e730da670702d1
Esther Kim <kim.esther117@gmail.com>
2023-01-20 21:49:09 -0800

    Created navbar dropdown feature

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Nav() {
   return (
@@ -11,32 +11,60 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">Home {'|'} </NavLink>
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/shoes">Shoes {'|'} </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/shoes/new">Create a New Shoe {'|'} </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/hats">Hats {'|'} </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/bins">Bins {'|'} </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/bins/new">Create a New Bin {'|'} </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/hats/new">Create a New Hat</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/locations">Locations</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/locations/new">Create a New Location</NavLink>
-            </li>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Shoes
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/shoes">Shoes</Link></li>
+                    <li><Link className="dropdown-item" to="/shoes/new">Create a New Shoe</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Bins
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/bins">Bins</Link></li>
+                    <li><Link className="dropdown-item" to="/bins/new">Create a New Bin</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Hats
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/hats">Hats</Link></li>
+                    <li><Link className="dropdown-item" to="/hats/new">Create a New Hat</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Locations
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><Link className="dropdown-item" to="/locations">Locations</Link></li>
+                    <li><Link className="dropdown-item" to="/locations/new">Create a New Location</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </ul>
         </div>
       </div>
